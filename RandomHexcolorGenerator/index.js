3ffb0bcbf7b7af39db9147bb4b2a8b7d82855b5d
David Lool <werder-fan89@gmx.de>
2020-09-16 17:23:53 +0200

    added the hex value under the heading

--- RandomHexcolorGenerator/index.js
+++ RandomHexcolorGenerator/index.js
@@ -1,14 +1,15 @@
 const btn = document.querySelector("#btn");
-const bodyy = document.querySelector("body");
-const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+const body = document.querySelector("body");
+const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const headingSecLine = document.querySelector("#headingSecLine");
 
 btn.addEventListener("click", function() {
   let hex = "#";
   for(let i = 0; i < 6; ++i) {
     hex += hexValues[generateRandomNumber()];
   }
-  console.log(hex);
-  bodyy.style.backgroundColor = hex;
+  body.style.backgroundColor = hex;
+  headingSecLine.textContent = hex;
 });
 
 function generateRandomNumber() {
