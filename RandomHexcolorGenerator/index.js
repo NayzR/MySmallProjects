f622fbffb7146b9dae0af4f07eeb75f32ff3d593
David Lool <werder-fan89@gmx.de>
2020-09-16 16:50:17 +0200

    finished js logic for random color generating

--- RandomHexcolorGenerator/index.js
+++ RandomHexcolorGenerator/index.js
@@ -1,13 +1,16 @@
 const btn = document.querySelector("#btn");
-const body = document.querySelector("body");
+const bodyy = document.querySelector("body");
 const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-console.log('hi');
+
 btn.addEventListener("click", function() {
   let hex = "#";
   for(let i = 0; i < 6; ++i) {
-    hex += hexValues[6];
+    hex += hexValues[generateRandomNumber()];
   }
   console.log(hex);
-  body.style.backgroundColor(hex);
+  bodyy.style.backgroundColor = hex;
 });
 
+function generateRandomNumber() {
+  return Math.floor(Math.random() * hexValues.length);
+}
